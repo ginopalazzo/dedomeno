54af5bc8cc7846d0312786f46f69aab2ef149e7f
Gino Palazzo <ginopalazzo@gmail.com>
2016-12-14 18:02:42 +0100

    additional files and changes for Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn mysite.wsgi
+web: gunicorn dedomeno.wsgi
---
+++ dedomeno/settingsOLD.py
@@ -0,0 +1,13 @@
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
