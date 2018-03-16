124cd0da9cc2a11f2a5a00956e6f6fc33f383788
ginopalazzo <ginopalazzo@gmail.com>
2018-03-16 19:25:22 +0100

    Finish idealista changes

--- setup.py
+++ setup.py
@@ -19,24 +19,12 @@ def read(*parts):
     return codecs.open(os.path.join(HERE, *parts), 'r').read()
 
 
-LONG_DESCRIPTION = read('README.rst')
-
-
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errcode = pytest.main(self.test_args)
-        sys.exit(errcode)
-
+LONG_DESCRIPTION = read('README.md')
 
 setup(
+    setup_requires=['pytest-runner'],
     name='dedomeno',
-    version=dedomeno.__version__,
+    version='0.1',
     url='http://github.com/ginopalazzo/dedomeno/',
     license='GNU General Public License v3.0',
     author='Gino Palazzo',
@@ -46,23 +34,23 @@ setup(
                       'celery>=4.1.0',
                       'django-bootstrap-breadcrumbs>=0.9.0',
                       'django-celery-beat>=1.1.<0',
-                      'django-multiselectfi<eld>=0.1.8',
+                      'django-multiselectfield>=0.1.8',
                       'fake-useragent>=0.1.8',
                       'flower>=0.9.2',
                       'python-decouple>=3.1',
                       'scrapy-djangoitem>=1.1.1',
                       ],
-    cmdclass={'test': PyTest},
+
     author_email='ginopalazzo@gmail.com',
     description='A Spanish real estate (Idealista) python scraper',
     long_description=LONG_DESCRIPTION,
     packages=['dedomeno'],
     include_package_data=True,
     platforms='any',
-    test_suite='dedomeno.test.test_dedomeno',
-    classifiers = [
+    test_suite='dedomeno.test',
+    classifiers=[
         'Programming Language :: Python',
-        'Development Status :: 4 - Beta',
+        'Development Status :: 1 - Alfa',
         'Natural Language :: English',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
